50e78a6bafdc669015d3ec4a91832784d9978aef
arun496 <arun496.in@gmail.com>
2022-12-24 13:12:54 +0530

    Project - react_webpack_setup - Setup CSS, Image resources

--- react_webpack_setup/src/App.tsx
+++ react_webpack_setup/src/App.tsx
@@ -1,3 +1,13 @@
+import './style.css';
+import imagePNG from './react.png';
+import imageSVG from './react.svg';
+
 export const App = () => {
-    return <h1>React Webpack Setup</h1>
+    return (
+        <>
+            <h1>React Webpack Setup</h1>
+            <img src={imagePNG} alt="React logo PNG" width="300" height="300" />
+            <img src={imageSVG} alt="React logo PNG" width="200" />
+        </>
+    )
 }
